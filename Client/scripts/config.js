031f2dd1756d16c228b3be5c14057097871e963f
Jarrad Whitaker <jmwhi13@student.monash.edu>
2015-03-24 22:07:02 +1100

    use current hostname instead of hardcode localhost

--- Client/scripts/config.js
+++ Client/scripts/config.js
@@ -2,7 +2,7 @@
 
 var kusemaConfigModule = angular.module('kusema.config', [])
   .constant('kusemaConfig', {
-    'host': 'localhost',
+    'host': window.location.hostname,
     'port': '3000',
     'url': function() { return 'http://'+this.host+':'+this.port+'/'; }
   });
