5e408e646520685d71b30fdb8818f8558e0acc2e
Jarrad Whitaker <jarrad@claude.winterst.internal>
2015-03-24 13:03:07 +1100

    fix typo

--- Client/scripts/services/SocketService.js
+++ Client/scripts/services/SocketService.js
@@ -1,7 +1,7 @@
 'use strict';
 
-kusema.factory('socketFactory', ['$rootScope', function ($rootScope, kusemaConfig) {
-  var socket = io.connect(kusemeConfig.url());
+kusema.factory('socketFactory', ['$rootScope', 'kusemaconfig', function ($rootScope, kusemaConfig) {
+  var socket = io.connect(kusemaConfig.url());
     return {
     
     on: function (eventName, callback) {
