26fbc4eea1f6d62e81fbf13ad170fa098490f0dd
Jarrad Whitaker <jarrad@claude.winterst.internal>
2015-03-24 22:00:09 +1100

    move config to a separate file for easier rebasing when app.js gets inevitably changed

--- Client/scripts/app.js
+++ Client/scripts/app.js
@@ -24,9 +24,3 @@ kusema.config(function($routeProvider) {
   });    
 });
 
-var kusemaConfigModule = angular.module('kusema.config', [])
-  .constant('kusemaConfig', {
-    'host': 'localhost',
-    'port': '3000',
-    'url': function() { return 'http://'+this.host+':'+this.port+'/'; }
-  });
