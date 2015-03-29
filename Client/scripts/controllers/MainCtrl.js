d46454c805e8aeeecfd1c71d67d8ea7e40c4d589
frank <frank@frank-virtual-machine>
2015-03-30 00:41:20 +1100

    merge new question controller

--- Client/scripts/controllers/MainCtrl.js
+++ Client/scripts/controllers/MainCtrl.js
@@ -17,14 +17,15 @@ kusema
 		[ '$scope', '$routeParams', '$timeout', 'questionFactory',
 				'toolboxFactory', function($scope, $routeParams, $timeout, questionFactory, toolboxFactory) {
 					var tmp = {};
-				    tmp.me = this;
-				    
-					tmp.allowMoreRequests = true; 
+					tmp.me = this;
+					this.allowMoreRequests = true;
+					this.writerOpen = false;
 					
 					tmp.me.questions = questionFactory.questions;
 					console.debug(tmp.me.questions);
 				    
-				    this.upVote = function(id) {
+					tmp.me.upVote = function(id, btn) {
+						console.debug(btn);
 				    	tmp.search = toolboxFactory.findObjectInArray(tmp.me.questions.questionList, '_id', id);
 				    	if(tmp.search.objectPosition !== -1) {
 				    		tmp.search.referenceToObject.upVotes++;
@@ -32,7 +33,7 @@ kusema
 				    	} else {alert('Invalid Question. (id: ' + id + ')')}
 				    }
 				    
-				    this.downVote = function(id) {
+					tmp.me.downVote = function(id) {
 				    	tmp.search = toolboxFactory.findObjectInArray(tmp.me.questions.questionList, '_id', id);
 				    	if(tmp.search.objectPosition !== -1) {
 				    		tmp.search.referenceToObject.downVotes++;
@@ -40,7 +41,7 @@ kusema
 				    	} else {alert('Invalid Question. (id: ' + id + ')')}
 				    }
 				    
-				    this.deleteQuestion = function(id) {
+					tmp.me.deleteQuestion = function(id) {
 				    	tmp.search = toolboxFactory.findObjectInArray(tmp.me.questions.questionList, '_id', id);
 				    	if(tmp.search.objectPosition !== -1) {
 				    		tmp.me.questions.questionList.splice(tmp.search.objectPosition, 1);
@@ -48,6 +49,39 @@ kusema
 				    	} else {alert('Invalid Question. (id: ' + id + ')')}
 				    }
 				    
+					tmp.me.closeWriter = function () {
+						tmp.me.writerOpen = false;
+				        $('.writer').animate({bottom:'-145px'}, 200);
+				        $('.contribute').animate({bottom:'55px', right:'90px', opacity: 1}, 200);
+				        $('#cross').css({'-webkit-transform' : 'rotate('+ 0 +'deg)',
+				         '-moz-transform' : 'rotate('+ 0 +'deg)',
+				         '-ms-transform' : 'rotate('+ 0 +'deg)',
+				         'transform' : 'rotate('+ 0 +'deg)'});
+				    };
+
+				    tmp.me.openWriter = function () {
+				    	tmp.me.writerOpen = true;
+				        $('.writer').animate({bottom:'0px'}, 200);
+				        $('.contribute').animate({bottom:'155px', right:'50%', opacity: 1}, 200);
+				        $('#cross').css({'-webkit-transform' : 'rotate('+ 45 +'deg)',
+				         '-moz-transform' : 'rotate('+ 45 +'deg)',
+				         '-ms-transform' : 'rotate('+ 45 +'deg)',
+				         'transform' : 'rotate('+ 45 +'deg)'});
+				    };
+				    
+				    tmp.me.addQuestion = function (data) {
+				    	tmp.newPost = {};
+				    	tmp.newPost.title = data.title;
+				        tmp.newPost.author = data.author;
+				        tmp.newPost.message = data.message;
+				        questionFactory.addQuestion(tmp.newPost).success(
+						function(response) {
+							tmp.me.closeWriter();
+							tmp.me.questions.questionList.push(response);
+						}).error(
+						function(error) {
+							$scope.status = 'Unable to add question: ' + error.message;
+						});
+				    }
 				} ])			
-				
 ;
