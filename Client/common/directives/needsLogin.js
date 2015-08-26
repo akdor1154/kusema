9763102ab3b9cc1921f7312a01fd40a38c573399
Jarrad Whitaker <jmwhi13@student.monash.edu>
2015-08-26 23:21:27 +1000

    add needs-user-id option to needs-login directive: <div kusema-needs-login needs-user-id="c.question.author">DELETE</div>

--- Client/common/directives/needsLogin.js
+++ Client/common/directives/needsLogin.js
@@ -3,19 +3,12 @@
 var needsLoginDirective = function() {
 	var linkFunction = function(scope, element, attributes) {
 		scope.c.needsLogin = !(attributes['kusemaNeedsLogin']=="false");
-
-		scope.$watch('c.needsLogin == (c.loginData.loginState == 1)', function(show, oldShow) {
-			console.log(scope);
-			if (show) {
-				element.removeClass('ng-hide');
-			} else {
-				element.addClass('ng-hide');
-			}
-		});
+		scope.c.element = element;
 	};
-	var compileFunction = function(element, attributes) {
-	}
 	return {
+		bindToController: {
+			needsUserId: '=',
+		},
 		scope: {},
 		restrict: 'A',
 		replace: false,
@@ -26,12 +19,47 @@ var needsLoginDirective = function() {
 	};
 }
 
-var needsLoginController = function(loginService) {
+var needsLoginController = function($scope, loginService) {
 		this.loginData = loginService.bindables;
+		$scope.$on('loginChanged', this.check.bind(this));
+		this.check();
 	}
 	needsLoginController.prototype = Object.create(Object.prototype, {
-		'needsLogin': {writable: true, enumerable: false, default: true}
+		'element': {writable: true, enumerable: false, value: null},
+		'needsLogin': {writable: true, enumerable: false, value: true},
+		'needsUserId': {
+			get: function() {
+				return this._needsUser;
+			},
+			set: function(newUser) {
+				if (newUser && newUser._id) {
+					this._needsUser = newUser._id;
+				} else {
+					this._needsUser = newUser;
+				}
+				this.check();
+			}
+		}
 	});
+	needsLoginController.prototype.check = function() {
+		console.log('checking');
+		if (!this.element) {
+			return;
+		}
+		if (this.shouldBeShown()) {
+			this.element.removeClass('ng-hide');
+		} else {
+			this.element.addClass('ng-hide');
+		}
+	};
+	needsLoginController.prototype.shouldBeShown = function() {
+		var userResult = true;
+		if (this.needsUserId) {
+			userResult = (this.loginData.user && (this.loginData.user._id == this.needsUserId));
+		}
+		var loginResult = (this.needsLogin == (this.loginData.loginState == 1));
+		return loginResult && userResult;
+	}
 
 kusema.user.directive('kusemaNeedsLogin', needsLoginDirective)
-		   .controller('needsLoginController', ['loginService', needsLoginController]);
+		   .controller('needsLoginController', ['$scope', 'loginService', needsLoginController]);
