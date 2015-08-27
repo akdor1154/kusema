e6581faa81a3a4cbd819af157097e3e9bae99a74
Jarrad Whitaker <jmwhi13@student.monash.edu>
2015-08-27 13:19:41 +1000

    configure baucis

---
+++ Server/config/baucis.js
@@ -0,0 +1,11 @@
+var baucis = require('baucis');
+
+baucis.rest('BaseContent');
+baucis.rest('Question');
+baucis.rest('Answer');
+baucis.rest('Comment');
+baucis.rest('User');
+baucis.rest('Topic');
+baucis.rest('Group');
+
+module.exports = baucis;
