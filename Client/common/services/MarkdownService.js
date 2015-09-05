043bbf7f9e2b7166d81a5addf9b73d0351bbc414
Jarrad Whitaker <jmwhi13@student.monash.edu>
2015-09-05 16:59:52 +1000

    implement syntax highlighting of code blocks with highlightjs

--- Client/common/services/MarkdownService.js
+++ Client/common/services/MarkdownService.js
@@ -1,12 +1,21 @@
 'use strict';
 
-var MarkdownService = function() {
+var MarkdownService = function(hljs) {
 	console.log('markdown');
 	return window.markdownit({
 		'html': false,
 		'linkify': true,
 		'breaks': true,
+		'highlight': function(code, language) {
+			if (language && hljs.getLanguage(language)) {
+				try {
+					console.log('highlighting');
+					return hljs.highlight(language, code).value;
+				} catch (e) {}
+			}
+			return '';
+		}
 		});
 }
 
-kusema.factory('markdownService', MarkdownService);
+kusema.factory('markdownService', ['highlightService', MarkdownService]);
