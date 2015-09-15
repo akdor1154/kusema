cd07c0f0f770df208c1d5fc6cc100008b489797a
Jarrad Whitaker <jmwhi13@student.monash.edu>
2015-09-15 17:38:17 +1000

    implement <kusema-server-message message={{serverResponse}}>, to display messages on some server errors

---
+++ Client/common/components/ServerMessage/serverMessage.js
@@ -0,0 +1,50 @@
+var serverMessageDirective = function() {
+	return {
+		scope: {},
+		bindToController: {
+			'message': '='
+		},
+		templateUrl: 'common/components/ServerMessage/serverMessageTemplate.html',
+		controller: 'kusemaServerMessageController',
+		controllerAs: 'c',
+		css: 'common/components/ServerMessage/serverMessage.css'
+	};
+}
+
+
+var serverMessageController = function() {
+	console.log('message!!');
+	this.setMessageProperties();
+}
+
+serverMessageController.prototype = Object.create(Object.prototype, {
+	'_message': {writable: true, enumerable: false, value: null},
+	'message': {
+		get: function() {
+			return this._message;
+		},
+		set: function(newMessage) {
+			console.log('setting message');
+			this._message = newMessage;
+			this.setMessageProperties();
+		}
+	},
+	'type': {writable: true, enumerable: false, value: ''},
+	'messageText': {writable: true, enumerable: false, value: ''},
+});
+
+serverMessageController.prototype.setMessageProperties = function() {
+	if (!this.message) {
+		console.log('no error');
+		delete this.type;
+		delete this.messageText;
+	} else {
+		console.log('showing error');
+		this.type = this.message.type || 'error';
+		this.messageText = (this.message.data && this.message.data.message) || this.message.data || 'Server Problem';
+	}
+}
+
+kusema.addModule('components.serverMessage')
+      .directive('kusemaServerMessage', serverMessageDirective)
+      .controller('kusemaServerMessageController', serverMessageController);
