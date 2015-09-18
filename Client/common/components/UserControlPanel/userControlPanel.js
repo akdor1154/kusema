8b4d4ea049a228e4b8671001c0d9824044e33d6e
Jarrad Whitaker <jmwhi13@student.monash.edu>
2015-09-18 17:19:36 +1000

    start fleshing out the user control panel with a list of the user's groups!

--- Client/common/components/UserControlPanel/userControlPanel.js
+++ Client/common/components/UserControlPanel/userControlPanel.js
@@ -1,27 +1,46 @@
 var UserControlPanelDirective = function() {
 	return {
-		scope: {},
 		templateUrl: 'common/components/UserControlPanel/userControlPanelTemplate.html',
 		controller: 'userControlPanelController',
 		controllerAs: 'c'
 	}
 };
 
-var UserControlPanelController = function() {
+var UserControlPanelController = function($css, $scope, loginService) {
+	//this thing doesn't actually use a real directive, so the css plugin doesn't work
+	// automatically :(
+	$css.bind({href: 'common/components/UserControlPanel/userControlPanel.css'}, $scope);
+
+	this.ls = loginService;
+	this.allGroups = loginService.bindables.user.subscriptions.groups;
+	this.authcateGroups = loginService.bindables.user.authcateSubscriptions.groups;
+	this.manualGroups = loginService.bindables.user.manualSubscriptions.groups;
+	console.log('bottom sheet controller made');
 	return this;
 }
 UserControlPanelController.prototype = Object.create(Object.prototype);
 
-UserControlPanelController.prototype.showUserControlPanel = function() {
-	console.log('showing user cp');
+UserControlPanelController.prototype.showUserControlPanel = function($mdBottomSheet) {
+	$mdBottomSheet.show(UserControlPanelDirective());
 }
 
-var UserControlPanelConfig = function($rootScope) {
+var UserControlPanelConfig = function($rootScope, $mdBottomSheet) {
 	console.log('configuring usercp')
-	$rootScope.showUserControlPanel = UserControlPanelController.prototype.showUserControlPanel;
+	$rootScope.showUserControlPanel = UserControlPanelController.prototype.showUserControlPanel.bind(window, $mdBottomSheet);
 }
+
+
+var GroupListItemDirective = function() {
+	return {
+		templateUrl: 'common/components/UserControlPanel/groupListItemTemplate.html',
+		scope: true,
+		replace: true
+	}
+}
+
 
 kusema.addModule('components.userControlPanel')
       .directive('kusemaUserControlPanel', UserControlPanelDirective)
-      .controller('userControlPanelController', UserControlPanelController)
-      .run(['$rootScope', UserControlPanelConfig]);
+      .controller('userControlPanelController', ['$css', '$scope', 'loginService', UserControlPanelController])
+      .directive('kusemaGroupListItem', GroupListItemDirective)
+      .run(['$rootScope', '$mdBottomSheet', UserControlPanelConfig]);
