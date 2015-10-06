98b1017820d9ec9c74d5f13f63d79338aee0f3e8
Jarrad Whitaker <jmwhi13@student.monash.edu>
2015-10-07 01:49:51 +1100

    add some useful stuff to the interaction schema

--- Server/models/interaction.js
+++ Server/models/interaction.js
@@ -1,16 +1,24 @@
 var mongoose = require('mongoose');
-var objectId = mongoose.Schema.Types.ObjectId;
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var User = require('./user.js');
+var Question = require('./question.js');
+var BaseContent = require('./common/contentMethods.js').BaseContent;
+var weightings = require('../config/weightings.js').topicWeightings;
 
 // Schema definition
 var interactionSchema = mongoose.Schema({
-    user:           { type: objectId, ref: 'User', required: true },
-    action:         { type: String, required: true },
-    topics:         [{ type: objectId }],
-    date:           { type: Date, default: Date.now }
+    user:           { type: ObjectId, ref: 'User', required: true },
+    action:         { type: String, required: true, enum: ['post', 'read'] },
+    contentType:    { type: String, required: true, enum: ['Question', 'Comment', 'Answer'] },
+    content:        { type: ObjectId, ref: 'BaseContent', required: true},
+    topics:         [{ type: String, ref: 'Topic' }],
+    date:           { type: Date, required: true, default: Date.now }
 })
 
 // Indexes
-interactionSchema.index({ user: 1, dateCreated: -1 });
+interactionSchema.index({ user: 1, date: -1 });
+interactionSchema.index({ user: 1, contentType: 1});
+interactionSchema.index({ content: 1, date: -1});
 
 
 module.exports = mongoose.model('Interaction', interactionSchema);
