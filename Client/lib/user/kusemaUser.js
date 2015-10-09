2f74a9678e74d1f1b249e042762067533835a49c
Jarrad Whitaker <jmwhi13@student.monash.edu>
2015-10-09 13:31:55 +1100

    user style changes (check out the sidebar now!)

--- Client/lib/user/kusemaUser.js
+++ Client/lib/user/kusemaUser.js
@@ -4,6 +4,8 @@ import './questionList/questionList.js';
 import './questionFull/questionFull.js';
 import template from './kusemaUserTemplate.html';
 
+import {Injector} from 'kusema.js';
+
 var KusemaUserConfig = function($stateProvider) {
 	$stateProvider
 		.state('user.home', {
@@ -33,12 +35,15 @@ var KusemaUserDirective = function() {
 	};
 };
 
-var KusemaUserController = function($scope, $mdSidenav, loginService) {
-	console.log('yo')
+var I = new Injector('loginService', 'groupService', '$state');
+
+var KusemaUserController = function($scope, $mdSidenav) {
+	I.init();
 	this.$mdSidenav = $mdSidenav;
 	$scope.toggle = this.toggle.bind(this);
-	$scope.loginData = loginService.bindables;
-	console.log($scope.loginData);
+	$scope.loginData = I.loginService.bindables;
+	this.gs = I.groupService;
+	console.log($scope);
 	$scope.searchGroups = function() {
 		return $scope.loginData.subscriptions.groups;
 	}
@@ -49,10 +54,14 @@ KusemaUserController.prototype.toggle = function(id) {
 	console.log('sup');
 	this.$mdSidenav(id).toggle();
 }
+KusemaUserController.prototype.goToGroup = function(newGroup) {
+	if (!newGroup) return;
+	I.$state.go('user.group', {groupID: newGroup});
+}
 
 import kusema from 'kusema.js';
 
 export default kusema.addModule('kusema.user', ['ngMaterial'])
 	   .config(['$stateProvider', KusemaUserConfig])
 	   .directive('kusemaUser', KusemaUserDirective)
-	   .controller('kusemaUserController', ['$scope', '$mdSidenav', 'loginService', KusemaUserController]);
+	   .controller('kusemaUserController', ['$scope', '$mdSidenav', KusemaUserController]);
